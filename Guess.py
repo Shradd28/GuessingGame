a1d1905f6d2c8024d34e3f8b5c6c3bebe1ef364d
Shraddha Desale <65727195+Shradd28@users.noreply.github.com>
2021-08-12 17:54:42 +0530

    Update Guess.py

--- Guess.py
+++ Guess.py
@@ -1,22 +1,22 @@
 import random
 counter=1
 MAX_NO_OF_ATTEMPTS=6
-print("*************Start the game*************")
+print("\t*************Start the game*************")
 num = random.randrange(0,100)
 
 while counter<=MAX_NO_OF_ATTEMPTS:
     num1 = int(input("Please enter any number between 0-100: "))
     if(num1==num):
-        print("Congrats!!!You guessed it right in ",counter,"attempts!!!")
+        print("\t-----Congrats!!!You guessed it right in ",counter,"attempts!!!-----")
         break
     if(num1>num):
-        print("Answer is less than your entered value !")
+        print("\t-----Answer is less than your entered value-----")
         counter=counter+1
     else:
-        print("Answer is greater than your entered value !")
+        print("\t-----Answer is greater than your entered value-----")
         counter=counter+1
         
         
 else:
-    print("Hard Luck!Try again")  
-print("Finished!!")
+    print("\t-----Hard Luck!Try again-----")  
+print("\t-----Finished!!-----")
